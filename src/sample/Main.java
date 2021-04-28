4332f5fc632b5e265416b32694fa58a88781dea3
pepe05422 <davidamir03@gmail.com>
2021-04-28 05:03:00 +0200

    son las 5 de la mañana

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("tablero.fxml"));
+        primaryStage.setTitle("Conecta Cuatbroo!!!!");
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
