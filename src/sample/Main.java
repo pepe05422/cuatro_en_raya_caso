f3dd95fb68498857aa1e4b1df4bda62965564b36
IvanOstro <osy.ivn@gmail.com>
2021-04-28 18:12:59 +0200

    tablero.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("tablero.fxml"));
-        primaryStage.setTitle("Conecta Cuatbroo!!!!");
+        primaryStage.setTitle("Conecta Cuatro");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
