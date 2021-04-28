623a9d29bbb09afd3b7c1364c685e4cdcb42ba54
pepe05422 <davidamir03@gmail.com>
2021-04-29 00:02:50 +0200

    hasta la polla

--- src/sample/TableroController.java
+++ src/sample/TableroController.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class TableroController {
+
+    public void chooseMulti(ActionEvent actionEvent) {
+    }
+
+    public void choosePC(ActionEvent actionEvent) {
+    }
+
+    public void changeOpacity(MouseEvent mouseEvent) {
+
+    }
 }
