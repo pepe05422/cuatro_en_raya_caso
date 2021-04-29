a116537bd97a433f0780b177c296f667a8d6a510
pepe05422 <davidamir03@gmail.com>
2021-04-29 21:42:35 +0200

    Modificado el Main para mejor manejo de Escenas y implementada pero no testeada la autentificacion

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,37 @@
 package sample;
 
+import DBAccess.Connect4DAOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Connect4;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene Escena;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Tablero.fxml"));
-        primaryStage.setTitle("Conecta Cuatro");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+    public void start(Stage primaryStage) throws IOException {
+
+        Escena = new Scene(loadFXML("RegisterMenu"),1200, 800);
+        primaryStage.setScene(Escena);
         primaryStage.show();
     }
+
+    public static void setRoot(String fxml) throws IOException {
+        Escena.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
