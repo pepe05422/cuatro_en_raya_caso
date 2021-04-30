0b84a4ca2ebb749ddd9586809329c92f83c99921
IvanOstro <osy.ivn@gmail.com>
2021-04-30 16:27:59 +0200

    edit1.2.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     private static Scene Escena;
+    private static TableroController tablero;
 
 
     @Override
@@ -24,12 +25,17 @@ public class Main extends Application {
     }
 
     public static void setRoot(String fxml) throws IOException {
-        Escena.setRoot(loadFXML(fxml));
+        //FXMLLoader root = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        Escena = new Scene(loadFXML("Tablero"),1200,800);
+
     }
 
     private static Parent loadFXML(String fxml) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        GridPane pantallaPrincipal = fxmlLoader.load();
+        tablero = fxmlLoader.getController();
+        tablero.crearContenido();
+        return pantallaPrincipal;
     }
 
     public static void main(String[] args) {
