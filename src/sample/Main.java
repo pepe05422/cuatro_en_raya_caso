1eaa3d027468c00f6341d0592215c84c2242463f
pepe05422 <davidamir03@gmail.com>
2021-04-30 18:06:11 +0200

    esto no funciona

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import DBAccess.Connect4DAOException;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,11 +21,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Escena = new Scene(loadFXML("Tablero"),1200, 800);
+
         primaryStage.setScene(Escena);
         primaryStage.setTitle("Conecta 4");
         primaryStage.setResizable(true);
-        primaryStage.setMinWidth(1100);
-        primaryStage.setMinHeight(598);
         primaryStage.show();
     }
 
@@ -33,10 +33,25 @@ public class Main extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
-        GridPane pantallaPrincipal = fxmlLoader.load();
+        if (fxml == "RegisterMenu") {
+            return loadRegistro();
+        }
+        if (fxml == "Tablero") {
+            return loadTablero();
+        }
+        return null;
+    }
+
+    public static Parent loadRegistro() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("RegisterMenu.fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static Parent loadTablero() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Tablero.fxml"));
+        tablero = (TableroController) fxmlLoader.getController();
         tablero.crearContenido();
-        return pantallaPrincipal;
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
